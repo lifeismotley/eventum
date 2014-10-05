df3e541deeab835e5efa51957feb4e216e31022a
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-10-04 21:53:10 -0400

    document the lib folder

--- app/lib/networking.py
+++ app/lib/networking.py
@@ -1,8 +1,19 @@
 from flask import make_response
 import json
 
-def response_from_json(dictionary, code):
-    response = make_response(json.dumps(dictionary), code)
+def response_from_json(data, code):
+    """Return a :class:`flask.Response` object ``data`` in the body, JSON
+    encoding it if necessary.
+
+    :param data: The data to be put in the response body.
+    :type data: str or dict
+    :param int code: The HTTP status code for the response.
+
+    :returns: the response object
+    :rtype: :class:`flask.Response`
+    """
+
+    response = make_response(json.dumps(data), code)
     response.headers['Content-Type'] = 'application/json'
     return response
 
